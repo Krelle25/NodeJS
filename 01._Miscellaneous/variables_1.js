ced19e376f584136e0783035e9a06cdb124020a8
Kristian Meyer Steffensen <krst0003@stud.ek.dk>
2026-01-29 10:33:04 +0100

    Added the variables_1 file

---
+++ 01._Miscellaneous/variables_1.js
@@ -0,0 +1,35 @@
+// Node REPL
+
+// type coercion
+// strict equality
+
+// Rule 1: use const per default unless it need to change, then use let
+
+// const is constant in the assignment
+
+const luckyNumber = 4;
+
+console.log(luckyNumber);
+
+// JS Data types
+// String, Boolean, Number, BigInt, Object, Symbol, null, undefined
+// Difference between JSON and a JS-object is that: JSON uses "" on the key while JS-object doesnt use "" on the key
+
+const person = {
+    // key-value pair
+    name: "Kristian"
+};
+
+person.name = "Knud"
+
+person.age = 100;
+
+console.log(person);
+
+const people = [];
+
+people.push("Krelle");
+
+console.log(people);
+
+// ASI = Automatic Semicolon Insertion
